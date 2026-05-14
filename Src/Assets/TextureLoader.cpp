778826970acac653411819398e75edaceb07971a
hotdogwithdog <hotdogwithdog@gmail.com>
2026-05-14 15:57:43 +0200

    Initial Commit

---
+++ Src/Assets/TextureLoader.cpp
@@ -0,0 +1,6 @@
+#include "TextureLoader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
---
+++ Src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "FGS Started\n";
+
+    return 0;
+}
